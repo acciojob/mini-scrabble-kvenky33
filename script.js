fe6119f1a84b097a516a145895854758c1bd95a8
konalam venkatesh <104829516+kvenky33@users.noreply.github.com>
2023-09-13 11:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+function text() {
+	let textname = document.getElementById("evaluatedText").value;
+	let length=textname.length;
+	let count = document.getElementById("letterCount");
+	count.textContent = length;
+}
